14f4d450e5847fff8f17d88c9f5bfcf1d544aab6
valadao-davi <davi.raichu@gmail.com>
2025-01-21 18:29:33 -0300

    feat: Project Created

---
+++ src/main/java/com/valadao_davi/gamesList/GamesListApplication.java
@@ -0,0 +1,13 @@
+package com.valadao_davi.gamesList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valadao_davi/gamesList/GamesListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valadao_davi.gamesList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
