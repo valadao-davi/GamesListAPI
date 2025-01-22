d6a2452af6dc18e12d4003276903ba88bfcd872d
valadao-davi <davi.raichu@gmail.com>
2025-01-22 02:26:17 -0300

    updated get game by id

--- src/main/java/com/valadao_davi/gamesList/controllers/GameController.java
+++ src/main/java/com/valadao_davi/gamesList/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.valadao_davi.gamesList.controllers;
 
+import com.valadao_davi.gamesList.dto.GameDTO;
 import com.valadao_davi.gamesList.dto.GameMinDTO;
 import com.valadao_davi.gamesList.entities.Game;
 import com.valadao_davi.gamesList.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +26,9 @@ public class GameController {
     }
 
     @GetMapping(value = "/{id}")
-    public Game findById(@PathVariable Long id){
-        return  gameService.findOne(id);
+    public ResponseEntity<GameDTO> findById(@PathVariable Long id){
+        GameDTO gameDTO = gameService.findById(id);
+        return ResponseEntity.ok(gameDTO);
     }
 
 }
--- src/main/java/com/valadao_davi/gamesList/services/GameService.java
+++ src/main/java/com/valadao_davi/gamesList/services/GameService.java
@@ -1,10 +1,12 @@
 package com.valadao_davi.gamesList.services;
 
+import com.valadao_davi.gamesList.dto.GameDTO;
 import com.valadao_davi.gamesList.dto.GameMinDTO;
 import com.valadao_davi.gamesList.entities.Game;
 import com.valadao_davi.gamesList.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,16 +16,16 @@ public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
-        return result.stream().map(x-> new GameMinDTO(x)).toList();
+        return result.stream().map(GameMinDTO::new).toList();
     }
 
-    public Game findOne(Long id){
-        Optional<Game> optionalGame = gameRepository.findById(id);
-        if(optionalGame.isPresent()){
-            return  optionalGame.get();
-        }
-        throw new RuntimeException("Game not found");
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
     }
 }
